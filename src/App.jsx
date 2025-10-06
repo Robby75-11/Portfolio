54c960b6e2e861623095b7bbacfa16539f267aae
Roberto Albergo <ralbergo7@gmail.com>
2025-10-06 20:51:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
       description:
         "Una piattaforma per la pianificazione di viaggi, che aiuta gli utenti a esplorare destinazioni e organizzare itinerari. Sviluppata per offrire un'esperienza utente fluida e ricca di funzionalità.Realizzata con React, Bootstrap,Javascript, Api Google Maps, Java, Spring Boot, Database PostgreSQL. Attendete giusto il tempo di un caffè e l'app si aprirà completamente",
       imageUrl: "/Screenshot 2025-07-25 165206.png",
-      projectUrl: "https://travel-mate-sand.vercel.app/",
+      projectUrl: "#",
       githubUrl: "https://github.com/Robby75-11/Travel-mate",
     },
     {
